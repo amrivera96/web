2883de9d3f57765bf14e7a7c3ebec3edfd891bbe
amrivera96 <61760607+amrivera96@users.noreply.github.com>
2024-04-15 18:55:08 -0600

    Add files via upload

---
+++ SSLFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace tallerMVC
+{
+    public class SSLFilter : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!filterContext.HttpContext.Request.IsSecureConnection)
+            {
+                var url = filterContext.HttpContext.Request.Url.ToString().Replace("http:", "https:");
+                filterContext.Result = new RedirectResult(url);
+            }
+        }
+    }
+}
